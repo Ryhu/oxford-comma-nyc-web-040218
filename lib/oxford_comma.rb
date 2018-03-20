c1f06cad8c45a563033a8ea6aa4390a43c60949f
Ryan Hu <ryanhuuu@gmail.com>
2018-03-20 22:03:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(arr)
+  if arr.length=1
+    return arr[0] + ", and " + arr[1]
+  elsif arr.length=2
+    return arr[0]
+  end
+  
   back = arr.pop
   return arr.join(", ") + ", and " + back
 end
